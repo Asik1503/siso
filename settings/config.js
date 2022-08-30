93042ee395c76887fc89c44727d7cfcab05ef3df
Asik1503 <102341195+Asik1503@users.noreply.github.com>
2022-08-30 17:21:27 +0300

    sisi

---
+++ settings/config.js
@@ -0,0 +1,51 @@
+module.exports.token = "OTU4MTc4ODI3ODc4NjcwNDE3.Gcqt7h.axHq9OEJL5ruXyraKBErvdrXNZ_YKJSRV7zMeI"
+
+module.exports.admins = ["907065641649991721"];
+
+module.exports.dataURL = "mongodb+srv://rimuru:Asked137@cluster0.w0tnx2c.mongodb.net/?retryWrites=true&w=majority"
+
+module.exports.inviteURL = "https://discord.com/oauth2/authorize?client_id=958178827878670417&permissions=8&scope=bot"
+
+module.exports.prefix = "!"
+
+module.exports.timelyReward = 50
+
+module.exports.timelyCooldown = "12h"
+
+module.exports.shopSortType = 0
+
+module.exports.bannerPrice = 500
+
+module.exports.memberRole = "997566966032240785"
+
+module.exports.welcomeChannel = "1011306071236821072"
+
+module.exports.mainGuild = "997566965994508359"
+
+module.exports.reactionPrice = 35
+
+module.exports.reportChannel = "1011306123644633198"
+
+module.exports.helpChannel = "1011306075926036510"
+
+module.exports.banRole = "1011305953523675246"
+
+module.exports.newAccountRole = "1013822126874165339"
+
+module.exports.eventBanRole = "1011305956518416485"
+
+module.exports.tmuteRole = "1011305954236694559"
+
+module.exports.vmuteRole = "1011305955511775313"
+
+module.exports.marryPrice = 1000;
+
+module.exports.marryParent = "1011306034930929714";
+
+module.exports.marryRole = "1011306015268012184";
+
+module.exports.marryTime = 180000;
+
+module.exports.primeRole = "1011305935026790450"
+
+module.exports.channelParent = "1011306032934441110";
